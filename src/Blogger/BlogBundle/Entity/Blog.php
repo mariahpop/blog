6a601189750bb2ae2b9cc017c9effabb54ec0dfc
Pop Maria <maria.pop@wayfare.ro>
2016-10-04 15:38:58 +0300

    final part3

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -13,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="blog")
+ * @ORM\HasLifecycleCallbacks
  */
 class Blog
 {
@@ -247,5 +248,19 @@ class Blog
     public function getUpdated()
     {
         return $this->updated;
+    }
+    
+    public function __construct()
+    {
+        $this->setCreated(new \DateTime());
+        $this->setUpdated(new \DateTime());
+    }
+    
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setUpdatedValue()
+    {
+       $this->setUpdated(new \DateTime());
     }
 }
